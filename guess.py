3860b35d4ba21b58cb78e9997578dacefad20625
att1cu5 <atticusmgr27@rdcrs.ca>
2026-02-04 15:14:15 -0700

    Update guess.py

--- guess.py
+++ guess.py
@@ -23,3 +23,43 @@ print("tries ",tries-u-j-y-h-q-f-n)
 for o in range(3,10):
     print(o,o-1,o-2,o-3)
 print("tries ",tries-u-j-y-h-q-f-n-o) 
+for k in range(0,10):
+   print(1,1,0,k)
+print("tries ",tries-u-j-y-h-q-f-n-o-k)
+tries=10000
+for u in range(0,10):
+   print(u,u,u,u)
+print("tries ",tries-u)   
+for j in range(0,7):
+    print(j,j+1,j+2,j+3)
+print("tries ",tries-u-j)   
+for h in range(0,9):
+    print(h+1,0,0,0)
+print("tries ",tries-u-j-h)  
+for y in range(0,9):
+    print(0,y+1,0,0)
+print("tries ",tries-u-j-y-h) 
+for q in range(0,9):
+    print(0,0,q+1,0)
+print("tries ",tries-u-j-y-h-q) 
+for f in range(0,9):
+    print(0,0,0,f+1)
+print("tries ",tries-u-j-y-h-q-f) 
+for n in range(3,10):
+    print(n-3,n-2,n-1,n)
+print("tries ",tries-u-j-y-h-q-f-n) 
+for o in range(3,10):
+    print(o,o-1,o-2,o-3)
+print("tries ",tries-u-j-y-h-q-f-n-o) 
+for w in range(1,10):
+   print(1,1,w,0)
+print("tries ",tries-u-j-y-h-q-f-n-o-k-w)
+for e in range(1,10):
+   print(2,2,e,0)
+print("tries ",tries-u-j-y-h-q-f-n-o-k-w-e)
+for qa in range(1,10):
+   print(3,3,qa,0)
+print("tries ",tries-u-j-y-h-q-f-n-o-k-w-e-qa)
+for wa in range(1,10):
+   print(9,9,wa,0)
+print("tries ",tries-u-j-y-h-q-f-n-o-k-w-e-qa-wa)
